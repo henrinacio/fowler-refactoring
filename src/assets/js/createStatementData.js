03cdd49d26e5f640645e22758b06298c9989f4ae
Henrique de Godoy Inácio <henriquedegodoy@gmail.com>
2023-09-18 17:21:06 -0300

    Add get amount to Performance Calculator class

--- src/assets/js/createStatementData.js
+++ src/assets/js/createStatementData.js
@@ -42,27 +42,7 @@ export default function createStatementData (invoice, plays) {
   }
 
   function amountFor (aPerformance) {
-    let result = 0
-
-    switch (aPerformance.play.type) {
-      case 'tragedy':
-        result = 40_000
-        if (aPerformance.audience > 30) {
-          result += 1_000 * (aPerformance.audience - 30)
-        }
-        break
-      case 'comedy':
-        result = 30_000
-        if (aPerformance.audience > 20) {
-          result += 1_000 + 500 * (aPerformance.audience - 20)
-        }
-        result += 300 * aPerformance.audience
-        break
-      default:
-        throw new Error(`unknow type: ${aPerformance.play.type}`)
-    }
-
-    return result
+    return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount
   }
 
   function playFor (aPerformance) {
@@ -74,5 +54,29 @@ class PerformanceCalculator {
   constructor (aPerformance, aPlay) {
     this.performance = aPerformance
     this.play = aPlay
+  }
+
+  get amount () {
+    let result = 0
+
+    switch (this.play.type) {
+      case 'tragedy':
+        result = 40_000
+        if (this.performance.audience > 30) {
+          result += 1_000 * (this.performance.audience - 30)
+        }
+        break
+      case 'comedy':
+        result = 30_000
+        if (this.performance.audience > 20) {
+          result += 1_000 + 500 * (this.performance.audience - 20)
+        }
+        result += 300 * this.performance.audience
+        break
+      default:
+        throw new Error(`unknow type: ${this.play.type}`)
+    }
+
+    return result
   }
 }
